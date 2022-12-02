7db45991625bb6942b9718c529e0b0912011bda2
Nin Pasker <gooddev.e.nin@gmail.com>
2022-12-02 11:04:30 -0500

    Added more slides to carosuel

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 20,
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
