3003a075868c48945784d1b7a5492214cdce8772
Nin Pasker <gooddev.e.nin@gmail.com>
2022-12-10 03:32:19 -0500

    changed carousel logic

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let swiper = new Swiper(".slide-content", {
-    slidesPerView: 1,
-    spaceBetween: 100,
-    slidesPerGroup: 1,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true', 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+const slider = document.querySelector('.slider');
+
+const leftArrow = document.querySelector('.left');
+
+const rightArrow = document.querySelector('.right');
+const indicatorParent = document.querySelector('.controls ul');
+
+let sectionIndex = 0;
+
+document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
+    indicator.addEventListener('click', function() {
+        sectionIndex = ind;
+        document.querySelector('.controls .selected').classList.remove('selected');
+        indicator.classList.add('selected');
+        slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+    });
+});
+
+
+rightArrow.addEventListener('click', function() {
+    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParent.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+})
+
+leftArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParent.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
 })
